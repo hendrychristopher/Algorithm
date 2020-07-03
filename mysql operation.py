9a40762cc95328efc6bff326f87db2454ca905fd
hendry christopher <58873776+hendrychristopher@users.noreply.github.com>
2020-07-03 13:38:54 +0530

    mistakenly added

--- mysql operation.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(host='localhost', user='root', passwd='12345', database='hendry')
-myCursor = mydb.cursor()
-myCursor.execute('select * from employee')
-result = myCursor.fetchall()
-for i in result:
-    print(i)
-print('bye')
-print('bye')
